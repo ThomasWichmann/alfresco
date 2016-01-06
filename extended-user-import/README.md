5049675741219ce2333576c7a4b788bfa8ed89bd
Thomas Wichmann <beren.wichmann@web.de>
2016-01-06 17:24:11 +0100

    cleanup

--- extended-user-import/README.md
+++ extended-user-import/README.md
@@ -2,3 +2,14 @@
 
 Created from standard Alfresco repository AMP archetype `mvn archetype:generate -DgroupId=de.alphazentauri -DartifactId=extended-user-import -Dversion=1.0-SNAPSHOT  -Dfilter=org.alfresco:`
 
+## Example call
+
+curl -F "file=<./src/test/resources/example-users.csv" -F isDryRun=true  "http://admin:admin@localhost:8080/alfresco/service/extended-user-import"
+
+## Building
+
+Run Alfresco locally with AMP loaded:
+mvn install -Pamp-to-war
+
+Rapid webscript reloading locally for testing:
+mvn compile alfresco:refresh-repo
---
+++ extended-user-import/src/main/amp/config/alfresco/extension/templates/webscripts/extended-user-import.post.js
@@ -0,0 +1,75 @@
+var ExtendedUserImport = {
+	
+	isDryRun: false,
+	csvDelimiter: ",",
+	
+	run: function() {
+	
+	var csvContent = null;
+	for each (field in formdata.fields)
+	{
+	  if (field.name == "file")
+	  {
+	      // this.response.message = field.filename + new
+		// String(field.content);
+	      csvContent = field.value;
+	  } else if (field.name == "isDryRun")
+	  {
+	      this.isDryRun = field.value;
+	  }
+	  this.response.message += field.name + "=" + field.value;
+	}
+	
+	// Create a regular expression to parse the CSV values.
+	var csvPattern = new RegExp(
+	            (
+	                // Delimiters.
+	                "(\\" + this.csvDelimiter + "|\\r?\\n|\\r|^)" +
+	                // Quoted fields.
+	                "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+	                // Standard fields.
+	                "([^\"\\" + this.csvDelimiter + "\\r\\n]*))"
+	            ),
+	            "gi"
+	            );
+	var csvData = [[]];
+	var matchingGroups = null;
+	while (matchingGroups = csvPattern.exec( csvContent )){
+	    if (matchingGroups.length<=1) {
+		continue;
+	    }
+	    var strMatchedDelimiter = matchingGroups[ 1 ];
+	    if (
+		    strMatchedDelimiter.length &&
+		    (strMatchedDelimiter != this.csvDelimiter)
+	    ){
+		// new row
+		csvData.push( [] );
+	    }
+	    // value (quoted or unquoted).
+	    if (matchingGroups[ 2 ]){
+		var strMatchedValue = matchingGroups[ 2 ].replace(
+			new RegExp( "\"\"", "g" ),
+	                    "\""
+	                    );
+	    } else {
+		// non-quoted value.
+		var strMatchedValue = matchingGroups[ 3 ];
+	    }
+
+	    csvData[ csvData.length - 1 ].push( strMatchedValue );
+	}
+	this.response.csvData = csvData;
+	
+	this.response.success = true;
+    },
+    response: {
+	success: false,
+    	message: null
+    },
+
+}
+
+ExtendedUserImport.run();
+
+model["response"] = ExtendedUserImport.response;
